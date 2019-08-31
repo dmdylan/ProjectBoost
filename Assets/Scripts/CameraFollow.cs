b7845d0da7550f31b66f98fd4419dfea5baa41d4
dmdylan <dylandanowski@gmail.com>
2019-08-31 11:10:16 -0400

    cleaned up some code, made camera follow player/rocket, changed lighting

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] float zOffset;
+    [SerializeField] float yOffset;
+    GameObject target;
+    Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+        target = GameObject.Find("Rocket Ship");
+    }
+
+    void Update()
+    {
+        Vector3 rocketInfo = target.transform.position;
+        mainCamera.transform.position = new Vector3(rocketInfo.x, 23, rocketInfo.z - zOffset);
+    }
+}
--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Oscillator : MonoBehaviour
 {
     [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
-    [Range(-1,1)] [SerializeField] float movementFactor; //0 for not moved, 1 for fully moved
+    [Range(0,1)] [SerializeField] float movementFactor; //0 for not moved, 1 for fully moved
     [SerializeField] float period = 4f;
 
     Vector3 startingPos;
@@ -23,10 +23,10 @@ public class Oscillator : MonoBehaviour
         //set movement factor
         if(period <= Mathf.Epsilon) { return; }
         float cycles = Time.time / period; //grows from 0
-        const float tau = Mathf.PI * 2;
+        const float tau = Mathf.PI * 2f;
         float rawSinWave = Mathf.Sin(cycles * tau);
 
-        movementFactor = rawSinWave;
+        movementFactor = rawSinWave /2f + .5f;
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
