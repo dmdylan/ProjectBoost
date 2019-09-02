176bb7068dcd6f7b95bba3f42b709234159611a6
dmdylan <dylandanowski@gmail.com>
2019-09-02 01:20:48 -0400

    changing terrain, need to add timer

---
+++ Assets/Scripts/LevelTimer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTimer : MonoBehaviour
+{
+    private TextAsset text;
+    Time 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<TextAsset>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
